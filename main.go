9c454217539f6f96944d92c08eddf861b8f2fa90
simon-beausire <simon.beausire@ynov.com>
2025-11-21 11:28:16 +0100

    Change background color in ColorHandler to black

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "route: %q", r.URL.Path)
 }
 func ColorHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #000000;}</style>")
 }
 
 func main() {
